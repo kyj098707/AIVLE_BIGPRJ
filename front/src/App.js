c61a5ada13ec90059b05c056e7f3cd40aea0373d
seojong789 <seojong789@naver.com>
2023-06-05 14:37:12 +0900

    FEAT: 폴더 구조 변경

--- front/src/App.js
+++ front/src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import Contents from "./components/Contents";
 import Footer from "./components/Footer";
 
+
 function App() {
   return (
     <div className="wrapper">
@@ -15,6 +16,9 @@ function App() {
         <Contents />
       </div>
       <Footer />
+
+
+      
     </div>
   );
 }
