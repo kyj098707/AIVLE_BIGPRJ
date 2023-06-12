f989c33e54e5faf0961f03765e0f03befb0e6f6e
kyj <kyj098707@naver.com>
2023-06-12 21:29:56 +0900

    FEAT: 그룹 모듈화

---
+++ front/src/components/group/GroupAward.js
@@ -0,0 +1,17 @@
+import '../../css/group/group.css'
+import { useParams } from "react-router-dom";
+import { React, useEffect, useState } from "react";
+import axios from 'axios';
+import { UserOutlined, CrownOutlined, MailOutlined } from '@ant-design/icons';
+import { Avatar, Card, Table, Menu, Input, Button, Modal, Badge } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+export default function GroupAward() {
+    
+  return (
+    <div>
+        Award 수상
+    </div>
+    
+  );
+}
--- front/src/components/group/GroupDetail.js
+++ front/src/components/group/GroupDetail.js
@@ -1,35 +1,17 @@
 import '../../css/group/group.css'
 import { useParams } from "react-router-dom";
-import { React, useEffect, useState } from "react";
-import axios from 'axios';
-import { UserOutlined, CrownOutlined, MailOutlined } from '@ant-design/icons';
-import { Avatar, Card, Table, Menu, Input, Button, Modal, Badge } from 'antd';
-
+import { React, useState } from "react";
+import {CrownOutlined} from '@ant-design/icons';
+import { Avatar, Card, Menu } from 'antd';
+import GroupMember from "./GroupMember"
+import GroupProblem from './GroupProblem';
+import GroupAward from './GroupAward'
 
 export default function GroupDetail() {
   const { id } = useParams();
-  const [member, setMember] = useState([])
-  const apiUrl = `http://localhost:8000/api/team/${id}/`;
   const [teamDetail, setTeamDetail] = useState("");
   const [current, setCurrent] = useState('member');
-  const [name, setName] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [nameError, setNameError] = useState('');
-  const [numReq, setNumReq] = useState(0);
-  const [reqList, setReqList] = useState([]);
-
-  var users = []
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
+  const [curContent, setCurContent] = useState(0);
   const items = [
     {
       //멤버들
@@ -38,8 +20,8 @@ export default function GroupDetail() {
     },
     {
       // 랭킹
-      label: 'Ranking',
-      key: 'ranking',
+      label: 'Award',
+      key: 'award',
     },
     {
       // 문제
@@ -47,115 +29,20 @@ export default function GroupDetail() {
       key: 'problem',
     }
     ,];
-  const columns = [
-    {
-      title: 'Position',
-      dataIndex: 'position',
-    },
-    {
-      title: 'Name',
-      dataIndex: 'name',
-    },
-    {
-      title: 'Solved',
-      dataIndex: 'solved',
-    },
-    {
-      title: 'Tier',
-      dataIndex: 'tier',
-    },
-  ];
-  const onChangeName = (event) => {
-    setName(event.target.value);
-    if (event.target.value !== "") {
-      setNameError("")
-    }
-  };
-
-  const requestClick = (userId, e) => {
-    const token = localStorage.getItem("access")
-    const headers = {
-      'Authorization': `Bearer ${token}`
-    }
-
-    axios.post(`http://localhost:8000/api/team/${id}/users/${userId}/`, {}, { headers: headers })
-      .then(response => {
-        console.log(response);
-        const {data} = response;
-        const {reqs,members} = data;
-        setReqList(reqs);
-        setMember(members);
-        setNumReq(numReq-1);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  useEffect(() => {
-    const token = localStorage.getItem("access")
-    const headers = {
-      'Authorization': `Bearer ${token}`
-    }
-    axios.get(`http://localhost:8000/api/team/${id}/users/list/`, { headers: headers })
-      .then(response => {
-        const { data } = response
-        setMember(data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
-    axios.get(`http://localhost:8000/api/team/${id}/req/list/`, { headers: headers })
-      .then(response => {
-        const { data } = response
-        setNumReq(data.length);
-        setReqList(data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
-    axios.get(apiUrl, { headers: headers })
-      .then(response => {
-        const { data } = response
-        setTeamDetail(data)
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
-
-    // 유저 정보 불어오기
-  }, []);
   const onClick = (e) => {
-    console.log('click ', e);
     setCurrent(e.key);
-  };
-  const inviteMember = (event) => {
-    async function fn() {
-
-      const token = localStorage.getItem("access")
-      const headers = {
-        'Authorization': `Bearer ${token}`
-      }
-
-      const response = await axios.post(`http://localhost:8000/api/team/${id}/invite/`, {
-        "name": name
-      }, { headers: headers })
-      console.log(response)
+    switch (e.key) {
+      case "award":
+        return setCurContent(1)
+      case "problem":
+        return setCurContent(2)
+      default:
+        return setCurContent(0)
     }
-    fn();
-  }
+  };
   return (
-
     <div className="group_detail_all">
 
-      {member && member.map(m => {
-        const { position, solved, user } = m;
-        let tmp = { "position": position, "solved": solved, "tier": user.tier, "name": user.username };
-        users.push(tmp)
-      })}
       <div className='detail_sidebar'>
         <Card>
           <div className='detail_avatar'>
@@ -170,51 +57,18 @@ export default function GroupDetail() {
         </Card>
 
         <div className='detail_menu'>
-
           <Menu onClick={onClick} selectedKeys={[current]} mode="vertical" items={items} />
         </div>
       </div>
-      <div className='detail_contents'>
-        <div className='add_member'>
-          <div className='add_member_input' onChange={onChangeName} >
-            <Input placeholder="초대할 사람의 아이디를 입력해주세요" />
-          </div>
-          <div className='add_member_btn'>
-            <Button type="dashed" onClick={inviteMember}> 초대 </Button>
-          </div>
-          <div className='request_badge'>
-            <Badge count={numReq} onClick={showModal}>
-              <Avatar shape="square" size="medium" icon={<MailOutlined />} />
-            </Badge>
-            <Modal title="킹덤 가입" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-            <Card>
-              {
-                reqList && reqList.map(r => {
-                  console.log(r);
-                  const { user } = r
+      {
+        {
+          0: <GroupMember />,
+          1: <GroupAward />,
+          2: <GroupProblem />
 
-                  return (
-                    <div className='request_list'>
-                      <div>
-                        <div> 이름 : {user.username} </div>
-                        <div> 티어 : {user.tier} </div>
-                      </div>
-                      <div className='request_btn'>
-                        <Button size="large" onClick={(e) => {requestClick(user.pk, e) }}> 수락하기 </Button>
-                      </div>
-                    </div>);
-                })
-              }
-              </Card>
-            </Modal>
-          </div>
-        </div>
-        <div className='member_table'>
-          <Table columns={columns} dataSource={users} />
-        </div>
-      </div>
-
-
+        }[curContent]
+      }
     </div>
+
   );
 }
