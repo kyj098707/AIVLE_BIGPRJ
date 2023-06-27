809ad22ca2a0a2ae26cbff783ded9d37029f1039
seojong789 <seojong789@naver.com>
2023-06-27 13:33:03 +0900

    FEAT: 어워드 폼 css

--- front/src/components/temp/TempAward.js
+++ front/src/components/temp/TempAward.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../scss/temp.scss'
+// import '../../scss/temp.scss'
 
 export default function TempAward () {
   return (
@@ -14,42 +14,27 @@ export default function TempAward () {
       <article className="leaderboard">
       <div className='leaderboard-header'>
         <img src="/img/trophy-48.png" className='leaderboard__icon' alt="ssss" />
-        <h1 className="leaderboard__title"><span className="leaderboard__title--top">끈기왕?</span><span className="leaderboard__title--bottom">추가내용</span></h1>
+        <h1 className="leaderboard__title"><span className="leaderboard__title--top">끈기왕</span><span className="leaderboard__title--bottom">추가내용</span></h1>
       </div>
 
       <main className="leaderboard__profiles">
         <article className="leaderboard__profile">
+        <img src="/img/rank_1.gif" alt="Dustin Moskovitz" className="leaderboard__picture" />
           <span className="leaderboard__name">aaaaaaaaaa</span>
           <span className="leaderboard__value">35<span>개</span></span>
         </article>
 
         <article className="leaderboard__profile">
-          {/* <img src="https://randomuser.me/api/portraits/men/97.jpg" alt="Dustin Moskovitz" className="leaderboard__picture" /> */}
+        <img src="/img/rank_2.gif" alt="Dustin Moskovitz" className="leaderboard__picture" />
           <span className="leaderboard__name">aaaaaaaaaa</span>
           <span className="leaderboard__value">9<span>개</span></span>
         </article>
 
         <article className="leaderboard__profile">
-          {/* <img src="https://randomuser.me/api/portraits/women/17.jpg" alt="Sheryl Sandberg" className="leaderboard__picture" /> */}
+        <img src="/img/rank_3.gif" alt="Dustin Moskovitz" className="leaderboard__picture" />
           <span className="leaderboard__name">aaaaaaaaaa</span>
           <span className="leaderboard__value">1<span>개</span></span>
         </article>
-
-        <article className="leaderboard__profile">
-          {/* <img src="https://randomuser.me/api/portraits/women/17.jpg" alt="Sheryl Sandberg" className="leaderboard__picture" /> */}
-          <span className="leaderboard__name">aaaaaaaaaa</span>
-          <span className="leaderboard__value">1<span>개</span></span>
-        </article>
-
-        <article className="leaderboard__profile">
-          {/* <img src="https://randomuser.me/api/portraits/women/17.jpg" alt="Sheryl Sandberg" className="leaderboard__picture" /> */}
-          <span className="leaderboard__name">aaaaaaaaaa</span>
-          <span className="leaderboard__value">1<span>개</span></span>
-        </article>
-{/* 저장 */}
-        
-
-        {/* Add more profile articles here */}
       </main>
     </article>
       </div>
