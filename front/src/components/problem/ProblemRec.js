7cde3c9909a751ab445adfc55223d5956f848c1e
kangminsu-git <0168416@naver.com>
2023-06-29 00:50:33 +0900

    FIX: 문제집 추가 모달창 디자인 수정

--- front/src/components/problem/ProblemRec.js
+++ front/src/components/problem/ProblemRec.js
@@ -223,9 +223,8 @@ export default function ProblemRec() {
         onCancel={handleModalCancel}
         width={1050}
         footer={[
-          <Button key="back" onClick={handleModalCancel}>
-            닫기
-          </Button>
+          <Button key="back" onClick={handleModalCancel}
+          >닫기</Button>
         ]}
       >
         <Table 
