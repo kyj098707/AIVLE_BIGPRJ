a4fff7b5851ce01c1a6fe2df926a03ef219238d9
BINNI <tmakd04@naver.com>
2023-07-03 13:57:10 +0900

    FEAT:메인과 이용약관 변경

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -4,14 +4,15 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="main_footer">
+
       <div className="inner">
         <div className="info">
-          <img src="/img/newalgoking.png" alt="ALGOKING" className="logo" />
+          <img src="/img/newalgoking.png" alt="ALGOKING" className="logo3" />
           <div className="nav">
             <ul className="help">
               <li2><Link to="/use" style={{ color: "#888888" }}>이용약관</Link></li2>
               <li2><Link to="/privacy" style={{ color: "#888888" }}>개인정보처리방침</Link></li2>
-              <li2><Link to="/help" style={{ color: "#888888" }}>도움말</Link></li2>
+              <li2><Link to="/about" style={{ color: "#888888" }}>도움말</Link></li2>
             </ul>
             <h4>(주)알고킹 경기도 성남시 분당구 불정로 90 (정자동)</h4>
             <h4>Aivle 3기 여러분 지금까지 고생 많으셨습니다.</h4>
--- front/src/components/sections/Section4.js
+++ front/src/components/sections/Section4.js
@@ -25,18 +25,18 @@ function Section4() {
 
   return (
     <div className={`section4 ${animate ? 'animate' : ''}`}>
-      <div className="card-container">
+      <div className="card-containers">
         <div className={`card ${animate ? 'animate' : ''}`}>
-          <h3>AI가 추천해 주는 <br/>점수상승에 효과적인 문제</h3>
+          <h3>AI기반 문제 추천</h3>
           <p>최신 학습 이론과 AI 기술을 활용하여,<br/> 당신의 학습 상황과 필요에 맞게 학습 방법을 제공합니다.</p>
         </div>
         <div className={`card ${animate ? 'animate' : ''}`}>
           <h3>라이벌 기능으로 실력 향상</h3>
-          <p>더 열나게 하겠슴다</p>
+          <p>AI로 매칭된 경쟁적인 환경에서 서로의 코드와 접근 방식을 비교하고 배워 개발 역량의 성장에 도움이 됩니다.</p>
         </div>
         <div className={`card ${animate ? 'animate' : ''}`}>
-          <h3>게시판 기능으로 모르는 문제 해결</h3>
-          <p>더더 열나게 하겠슴다</p>
+          <h3>그룹을 통한 스터디 그룹 생성</h3>
+          <p>그룹 기능을 활용하여 도움과 피드백을 받으며 문제를 해결하는 과정에서 실력과 자신감이 크게 성장 할 수 있습니다.</p>
         </div>
       </div>
     </div>
