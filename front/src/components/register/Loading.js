36485f606c93be5683e110a8dfa1d140bed47267
seojong789 <seojong789@naver.com>
2023-07-04 10:37:22 +0900

    FEAT: 회원가입 시 로딩 모달 처리

--- front/src/components/register/Loading.js
+++ front/src/components/register/Loading.js
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../scss/Register.scss'
 // Modal 팝업 관련
 import Modal from 'react-modal'
 Modal.setAppElement('#root'); // 모달을 렌더링할 DOM 요소를 설정
 // Modal 팝업 관련
 
-export default function LoadingModal() {
+export default function LoadingModal(loadingModalVisible) {
   // Modal 팝업 관련
   const [isOpen, setIsOpen] = useState(false);
-  const [modalMsg, setModalMsg] = useState('에러입니다.');
   const openModal = () => {
     setIsOpen(true);
   };
   const closeModal = () => {
     setIsOpen(false);
   };
+
+  useEffect(() => {
+    if (loadingModalVisible) openModal()
+    console.log("use loadingModalVisible", loadingModalVisible)
+  }, [loadingModalVisible]);
+
   // Modal 팝업 관련
   const [open, setOpen] = useState(false);
   return (
     <>
-      <button onClick={openModal}>클릭</button>
       <Modal
         isOpen={isOpen}
         onRequestClose={closeModal}
@@ -49,16 +53,10 @@ export default function LoadingModal() {
       >
         <div className="loading-container">
           <div className="loading-text">
-            <span className="loading-text-words" style={{fontFamily:'Tenada'}}>L</span>
-            <span className="loading-text-words" style={{fontFamily:'Tenada'}}>O</span>
-            <span className="loading-text-words" style={{fontFamily:'Tenada'}}>A</span>
-            <span className="loading-text-words" style={{fontFamily:'Tenada'}}>D</span>
-            <span className="loading-text-words" style={{fontFamily:'Tenada'}}>I</span>
-            <span className="loading-text-words" style={{fontFamily:'Tenada'}}>N</span>
-            <span className="loading-text-words" style={{fontFamily:'Tenada'}}>G</span>
+            <span className="loading-text-words" style={{fontFamily:'Tenada'}}>AI와 연결 중...</span>
           </div>
           <p style={{color:'white', marginTop:'240px', textAlign:'center', fontFamily:'Pretendard-regular', textShadow: '2px 1px 2px yellow', fontSize:'20px'}}>알고계셨나요?</p>
-          <p style={{color:'white', textAlign:'center', fontFamily:'Pretendard-regular'}}>코딩 테스트를 합격하기 위해 준비하는 기간은 2달 이하입니다.</p>
+          <p style={{color:'white', textAlign:'center', fontFamily:'Pretendard-regular'}}>2달이면 코딩 테스트를 합격하기 충분한 시간입니다.</p>
         </div>
       </Modal>
   </>
