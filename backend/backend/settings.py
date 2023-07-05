29600ee8fef4ed5308049d57e13b24d4bba5fe4b
kyj <kyj098707@naver.com>
2023-07-05 14:27:13 +0900

    FIX: 버그 수정

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ django_config = config["DJANGO"]
 SECRET_KEY = django_config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
